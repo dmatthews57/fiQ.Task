bb59a065e8c06425d2440e20a19ea070f47a3d26
dmatthews57 <dmatthews57@gmail.com>
2020-12-16 10:42:05 -0500

    Add project files.

---
+++ fiQ.Task.Engine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace fiQ.Task.Engine
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Load application configuration:
+			var configbuilder = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+				.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true, reloadOnChange: false)
+				.AddEnvironmentVariables();
+			if (args != null)
+			{
+				configbuilder.AddCommandLine(args);
+			}
+			var config = configbuilder.Build();
+
+			// Build logger configuration from application config file and create logger:
+			Log.Logger = new LoggerConfiguration()
+				.ReadFrom.Configuration(config)
+				.CreateLogger();
+
+			// Set up services for dependency injection:
+			var serviceCollection = new ServiceCollection()
+				.AddLogging(configure => configure.AddSerilog())
+				.AddSingleton<IConfiguration>(config)
+			;
+
+			using (var serviceProvider = serviceCollection.BuildServiceProvider())
+			{
+			}
+		}
+	}
+}
---
+++ fiQ.Task.Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("fiQ.Task.Engine")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("fiQ DevOps Inc.")]
+[assembly: AssemblyProduct("fiQ.Task.Engine")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("D5F9FE32-036B-45D4-880A-ACE7ECD31DC5")]
+
+[assembly: AssemblyVersion("1.0.*")]
